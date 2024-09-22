7e29ad728bf5b076cb0e4b18dfb9695a9298b8b4
venajustin <67924865+venajustin@users.noreply.github.com>
2024-09-21 22:27:50 -0700

    minor line deletion

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,6 +2,5 @@ return {
 
 	{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
     { "nyoom-engineering/oxocarbon.nvim", name = "oxocarbon", lazy = false, priority = 1000 },
-
 }
 
