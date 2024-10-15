68185befb460b208b1fda9c6d92b51093e258eef
venajustin <venajustin@gmail.com>
2024-10-14 18:29:14 -0700

    line overflow and nice navigation of errors reported

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -18,7 +18,7 @@ vim.opt.backup = false
 vim.opt.undodir = vim.fn.stdpath('config') .. "\\undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = true 
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
@@ -29,3 +29,9 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+vim.opt.wrap = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+
