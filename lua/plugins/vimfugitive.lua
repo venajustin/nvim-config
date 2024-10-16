4b9569be7983c558a2f4109b5aa39cf9393e289f
venajustin <venajustin@gmail.com>
2024-10-16 11:08:28 -0700

    added git blame keybind

--- lua/plugins/vimfugitive.lua
+++ lua/plugins/vimfugitive.lua
@@ -3,7 +3,8 @@ return {
     opts = {},
     keys = {
         { "<leader>gs", "<cmd>Git<cr>", desc = "Git Tools" },
+        { "<leader>gb", "<cmd>Git blame<cr>", desc = "Git Blame" },
     },
-    config = function() 
+    config = function()
     end,
 }
