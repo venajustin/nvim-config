e88a9512795dbe53186ee7b5901fd83058c35c3c
venajustin <venajustin@gmail.com>
2025-08-10 15:39:23 -0700

    remove most of treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,26 +7,17 @@ return {
         require("nvim-treesitter.configs").setup({
             highlight = {
                 enable = true,
-			},
-			incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<CR>",
-                    scope_incremental = "<CR>",
-                    node_incremental = "<TAB>",
-                    node_decremental = "<S-TAB>",
-                }
-            },
-            ensure_installed = {
-                "javascript",
-                "c",
-                "cpp",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "html",
-                "css",
-            },
+	    },
+             ensure_installed = {
+--                 "javascript",
+--                 "c",
+--                 "cpp",
+--                 "lua",
+--                 "markdown",
+--                 "markdown_inline",
+--                 "html",
+--                 "css",
+             },
         })
     end,
 }
